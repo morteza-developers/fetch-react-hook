1f4375578ec3f9266b4d565b02bd43963c5b64cf
morteza22333 <morteza.mk14@gmail.com>
2023-10-26 11:25:40 +0330

    fix type export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import useFetch from "./hooks/useFetch";
 export * from "./provider";
-import * as FetchType from "./type";
+export * from "./type";
 
-export type { FetchType };
 export default useFetch;
